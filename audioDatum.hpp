a8d11c066fb92d85a13ea56e64b5e1aa5c689004
Reed Reese-Steiner <arleycat@gmail.com>
2018-03-09 21:06:43 -0800

    replace dynamically allocated arrays with std::vector in audioDatum and audioDatumFourier

--- audioDatum.hpp
+++ audioDatum.hpp
@@ -1,12 +1,15 @@
 #ifndef AUDIODATUM_HPP
 #define AUDIODATUM_HPP
 
+#include <vector>
+
 class audioDatum {
 	public: //private
 		unsigned int samples;
 		unsigned int samplerate;
 		int channels;
-		float** data; //array of channels, where each channel is an array of samples (floats)
+		//float** data; //array of channels, where each channel is an array of samples (floats)
+    std::vector<std::vector<float> > data;
 
 		audioDatum(void);
 		~audioDatum(void);
@@ -21,7 +24,7 @@ class audioDatum {
     void setSampleRate(unsigned int samplerate);
     int getChannels();
     void setChannels(int channels);
-    float** getData(); // is this safe?
+    std::vector< std::vector<float> > getData(); // is this safe? 
     float getSample(int channel, unsigned int sample_idx);
     void setSample(int channel, unsigned int sample_idx, float sample);
     void print_debug(); 
